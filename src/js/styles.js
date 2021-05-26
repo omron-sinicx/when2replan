d6ace87d22bde806c551e94fa231ffd4f26b5425
denkiwakame <denkivvakame@gmail.com>
2021-05-26 20:05:16 +0900

    added normalize.css / adjust margins

--- src/js/styles.js
+++ src/js/styles.js
@@ -1,5 +1,5 @@
 import "../scss/theme.scss";
-// import "../../node_modules/normalize.css/normalize.css";
+
 import authorIcon1 from "../images/potato.png";
 import authorIcon2 from "../images/shiromo.png";
 import teaserImg from "../images/teaser.jpeg";
