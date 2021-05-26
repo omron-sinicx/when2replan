4ad69b44c21900ba0ded2434ce2ab6d7b8964a2c
denkiwakame <denkivvakame@gmail.com>
2021-05-26 20:58:05 +0900

    add file-loader

--- src/js/styles.js
+++ src/js/styles.js
@@ -3,6 +3,9 @@ import "../scss/theme.scss";
 import authorIcon1 from "../images/potato.png";
 import authorIcon2 from "../images/shiromo.png";
 import teaserImg from "../images/teaser.jpeg";
+import resultImg1 from "../images/result1.png";
+import resultImg2 from "../images/result2.png";
+import methodImg from "../images/method.png";
 
 import UIkit from "uikit";
 import Icons from "uikit/dist/js/uikit-icons";
