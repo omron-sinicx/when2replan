c56157430a476db657388a2f456acaaa7c1f4042
denkiwakame <denkivvakame@gmail.com>
2021-05-31 13:51:14 +0900

    created: Resource React Component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,6 +6,30 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faGithub,faYoutube,faMedium} from '@fortawesome/free-brands-svg-icons';
 import {faFilePdf} from '@fortawesome/free-solid-svg-icons';
 
+class ResourceBtn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.icons = {
+      "paper": faFilePdf,
+      "code": faGithub,
+      "video": faYoutube,
+      "blog": faMedium,
+    }
+  }
+  render() {
+    if (!this.props.url) return null;
+    const aClass = this.props.title == "paper" ? `uk-button uk-button-text`: `uk-button uk-button-text uk-margin-medium-left`; // FIXME
+    return (
+      <>
+        <a className={aClass} href={this.props.url} target="_blank">
+          <FontAwesomeIcon icon={this.icons[this.props.title]} size="lg" color="#1C5EB8" />
+          <span className="uk-margin-small-left uk-margin-small-right uk-text-primary uk-text-bolder">{this.props.title}</span>
+        </a>
+      </>
+    );
+  }
+}
+
 export default class Header extends React.Component {
   constructor(props) { super(props); }
 
@@ -20,22 +44,7 @@ export default class Header extends React.Component {
           </div>
           <Authors authors={this.props.authors} affiliations={this.props.affiliations} meta={this.props.meta}/>
           <div className="uk-flex uk-flex-center uk-margin-top">
-            <a className="uk-button uk-button-text" href={this.props.paper} target="_blank">
-              <FontAwesomeIcon icon={faFilePdf} size="lg" color="#1C5EB8" />
-              <span className="uk-margin-small-left uk-margin-small-right uk-text-primary uk-text-bolder">paper</span>
-            </a>
-            <a className="uk-button uk-button-text uk-margin-medium-left" href={this.props.code} target="_blank">
-              <FontAwesomeIcon icon={faGithub} size="lg" color="#1C5EB8" />
-              <span className="uk-margin-small-left uk-margin-small-right uk-text-primary uk-text-bolder">code</span>
-            </a>
-            <a className="uk-button uk-button-text uk-margin-medium-left" href="#video">
-              <FontAwesomeIcon icon={faYoutube} size="lg" color="#1C5EB8" />
-              <span className="uk-margin-small-left uk-margin-small-right uk-text-primary uk-text-bolder">video</span>
-            </a>
-            <a className="uk-button uk-button-text uk-margin-medium-left" href={this.props.blog}>
-              <FontAwesomeIcon icon={faMedium} size="lg" color="#1C5EB8" />
-              <span className="uk-margin-small-left uk-margin-small-right uk-text-primary uk-text-bolder">blog</span>
-            </a>
+            {Object.keys(this.props.resources).map(key => <ResourceBtn url={this.props.resources[key]} title={key} />)}
           </div>
         </div>
       </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -40,7 +40,7 @@ class Template extends React.Component {
         />
         <div data-uk-sticky className="uk-visible@l">
           <ForkMeOnGitHub
-            repo={data.repo}
+            repo={data.resources.code}
             colorBackground="#999"
             colorOctocat="white"
           />
@@ -50,13 +50,10 @@ class Template extends React.Component {
                 authors={data.authors}
                 affiliations={data.affiliations}
                 meta={data.meta}
-                code={data.repo}
-                video={data.video}
-                paper={data.paper}
-                blog={data.blog}  />
+                resources={data.resources} />
         <div className="uk-container uk-container-small">
           <Overview overview={data.overview} />
-          <Video video={data.video} />
+          <Video video={data.resources.video} />
           <Method method={data.method} />
           <Result results={data.results} />
           <Contact authors={data.authors} contact_ids={data.contact_ids}/>
