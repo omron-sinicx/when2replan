d1b2fcc43369f5facea2faa706ff60bb65fc9aa6
denkiwakame <denkivvakame@gmail.com>
2021-05-31 17:19:21 +0900

    :art: created spakerdeck React Components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {render} from 'react-dom';
-import {Helmet} from "react-helmet";
+import React from "react";
+import { render } from "react-dom";
+import { Helmet } from "react-helmet";
 
 import Header from "../components/header.jsx";
 import Overview from "../components/overview.jsx";
@@ -10,32 +10,34 @@ import Result from "../components/results.jsx";
 import Contact from "../components/contact.jsx";
 import Footer from "../components/footer.jsx";
 import Citation from "../components/citation.jsx";
+import SpeakerDeck from "../components/speakerdeck.jsx";
 import ForkMeOnGitHub from "fork-me-on-github";
 
 import data from "../data/template.yaml";
 
 class Template extends React.Component {
   render() {
+    console.log(data);
     return (
       <div>
         <Helmet
           title={data.title}
           meta={[
-            { name: 'viewport', content: 'width=device-width,initial-scale=1'},
+            { name: "viewport", content: "width=device-width,initial-scale=1" },
             { property: "og:site_name", content: data.organization },
             { property: "og:type", content: "article" },
             { property: "og:title", content: data.title },
-            { property: "og:description", content: data.description},
-            { property: 'og:image', content: data.image}, // FIXME
-            { property: "og:image:width", content: "912"},
-            { property: "og:image:height", content: "618"},
-            { property: 'og:url', content: data.url},
-            { name: 'twitter:card', content: "summary_large_image"},
-            { name: 'twitter:title', content: data.title},
-            { name: 'twitter:image', content: data.image},
-            { name: 'twitter:description', content: data.description},
-            { name: 'twitter:url', content: data.url},
-            { name: 'twitter:site', content: data.twitter},
+            { property: "og:description", content: data.description },
+            { property: "og:image", content: data.image }, // FIXME
+            { property: "og:image:width", content: "912" },
+            { property: "og:image:height", content: "618" },
+            { property: "og:url", content: data.url },
+            { name: "twitter:card", content: "summary_large_image" },
+            { name: "twitter:title", content: data.title },
+            { name: "twitter:image", content: data.image },
+            { name: "twitter:description", content: data.description },
+            { name: "twitter:url", content: data.url },
+            { name: "twitter:site", content: data.twitter },
           ]}
         />
         <div data-uk-sticky className="uk-visible@l">
@@ -45,24 +47,27 @@ class Template extends React.Component {
             colorOctocat="white"
           />
         </div>
-        <Header title={data.title}
-                conference={data.conference}
-                authors={data.authors}
-                affiliations={data.affiliations}
-                meta={data.meta}
-                resources={data.resources} />
+        <Header
+          title={data.title}
+          conference={data.conference}
+          authors={data.authors}
+          affiliations={data.affiliations}
+          meta={data.meta}
+          resources={data.resources}
+        />
         <div className="uk-container uk-container-small">
           <Overview overview={data.overview} />
+          <SpeakerDeck dataId={data.speakerdeck} />
           <Video video={data.resources.video} />
           <Method method={data.method} />
           <Result results={data.results} />
-          <Contact authors={data.authors} contact_ids={data.contact_ids}/>
+          <Contact authors={data.authors} contact_ids={data.contact_ids} />
           <Citation bibtex={data.bibtex} />
         </div>
         <Footer />
       </div>
-    )
+    );
   }
 }
 
-render(<Template/>, document.getElementById('root'))
+render(<Template />, document.getElementById("root"));
