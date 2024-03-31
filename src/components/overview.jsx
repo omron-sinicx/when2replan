e150516282507099aedb10e424f0055dab73243a
Mai Nishimura <mai.nishimura@sinicx.com>
2024-03-31 10:35:27 +0900

    refactor source

--- src/components/overview.jsx
+++ src/components/overview.jsx
@@ -17,16 +17,14 @@ export default class Overview extends React.Component {
           height=""
           alt=""
         />
-        {this.props.description
-          ? [
-              <p className="uk-text-primary uk-text-center uk-margin-bottom">
-                <span className="uk-label uk-label-primary uk-text-center uk-text-bold">
-                  TL;DR
-                </span>{' '}
-                {this.props.description}
-              </p>,
-            ]
-          : ''}
+        {this.props.description && (
+          <p className="uk-text-primary uk-text-center uk-margin-bottom">
+            <span className="uk-label uk-label-primary uk-text-center uk-text-bold">
+              TL;DR
+            </span>{' '}
+            {this.props.description}
+          </p>
+        )}
         <h2>Overview</h2>
         <p>{this.props.overview}</p>
       </div>
